0c339ce32faa69d26f1a61cb5c4d20bb78a1ebe1
ebelbell <ebella2056@gmail.com>
2024-05-06 14:46:23 -0700

    first commit

---
+++ script.mjs
@@ -0,0 +1,15 @@
+/** This program advises a group of environmental scientists how to handle the growth 
+and spread of a unique plant species under their supervision. Develop a growth 
+control system that will monitor and predict the plant growth, based 
+on the available space and potential growth.
+
+Plant Info: plant containers are circular w/ a 5m radius.
+Calculate the area formula: 
+const PI = 3.1415;
+const area = PI * radius * radius;
+
+Other info: Each plant requires a minimum space of 0.8 square meters.
+The area is starting with 20 plants.
+The plants double in number every week.
+*/
+
